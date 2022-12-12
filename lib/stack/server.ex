9147b0242ab341b1f33cb70b68097ce671d81ee7
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2022-12-12 00:50:36 +0000

    implement push and peek

--- lib/stack/server.ex
+++ lib/stack/server.ex
@@ -9,4 +9,18 @@ defmodule Stack.Server do
     [elem | tail] = current_list
     {:reply, elem, tail}
   end
+
+  def handle_call(:peek, _from, current_list) do
+    [elem | _] = current_list
+    {:reply, elem, current_list}
+  end
+
+  def handle_cast({:push, elem}, current_list) do
+    {:noreply, [elem | current_list]}
+  end
 end
+
+# {:ok, pid} = GenServer.start_link(Stack.Server, [1,2,3])
+# GenServer.call(pid, :peek)
+# GenServer.call(pid, :pop)
+# GenServer.cast(pid, {:push, 200})
